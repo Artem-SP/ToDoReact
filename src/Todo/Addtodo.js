b73f60c042d67f226760ef972057b0956c74e813
Artem Sukhorukov <54895933+Artem-SP@users.noreply.github.com>
2020-09-17 13:45:12 +0300

    170920 13:44 (tm 1:00:00) work good

--- src/Todo/Addtodo.js
+++ src/Todo/Addtodo.js
@@ -2,20 +2,31 @@ import React, { useState } from "react";
 import ProrTypes from "prop-types";
 //import Context from "../context";
 
-function AddTodo(onCreate) {
-  const [value, setValue] = useState("");
+function useInputValue(defaultValue = "") {
+  const [value, setValue] = useState(defaultValue);
+  return {
+    bind: { value, onChange: (event) => setValue(event.target.value) },
+    clear: () => setValue(""),
+    value: () => value
+  };
+}
+
+function AddTodo({ onCreate }) {
+  const input = useInputValue("");
 
   function submitHandler(event) {
     event.preventDefault();
 
-    if (value.trim()) {
-      onCreate(value);
+    if (input.value().trim()) {
+      onCreate(input.value());
+      input.clear();
+      // setValue("");
     }
   }
 
   return (
     <form style={{ marginBottom: "1rem" }} onSubmit={submitHandler}>
-      <input value={value} onChange={(event) => setValue(event.target.value)} />
+      <input {...input.bind} />
       <button type="submit">Add Todo</button>
     </form>
   );
